4c700697be58c7781402bef1fb3d6e43a4d05c08
Stephan Bösebeck <sb@caluga.de>
2022-09-26 23:36:29 +0200

    making java develop work

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -42,11 +42,11 @@ keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 
--- Better paste 
+-- Better paste
 keymap("v", "p", '"_dP', opts)
 
 -- Insert --
--- Press jk fast to enter 
+-- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 
 -- Visual --
@@ -69,8 +69,8 @@ keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
 
 -- Comment
-keymap("n", "<leader>gcc", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
-keymap("x", "<leader>gcc", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
+--[[ keymap("n", "<leader>gcc", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts) ]]
+--[[ keymap("x", "<leader>gcc", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>') ]]
 
 -- DAP
 keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
@@ -82,3 +82,20 @@ keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
 keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
 keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
 keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+
+--[[ -- lsp ]]
+--[[ keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts) ]]
+--[[ keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) ]]
+--[[ keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts) ]]
+--[[ keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts) ]]
+--[[ keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts) ]]
+--[[ keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts) ]]
+keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
+--[[ keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts) ]]
+--[[ keymap("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts) ]]
+--[[ keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts) ]]
+--[[ keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts) ]]
+--[[ keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts) ]]
+--[[ keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts) ]]
+--[[ keymap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts) ]]
+--[[ keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts) ]]
