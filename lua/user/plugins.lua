9d71fd9c4d1cc452eb2dbefb4a98399c4929f477
Stephan Bösebeck <sb@caluga.de>
2022-09-27 09:07:45 +0200

    changes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -49,6 +49,8 @@ return packer.startup(function(use)
   use { "kyazdani42/nvim-tree.lua" }
   use { "akinsho/bufferline.nvim" }
   use { "moll/vim-bbye" }
+  use { 'DanilaMihailov/beacon.nvim' }               -- cursor jump
+
   use { "nvim-lualine/lualine.nvim" }
   use { "akinsho/toggleterm.nvim" }
   use { "ahmedkhalf/project.nvim" }
