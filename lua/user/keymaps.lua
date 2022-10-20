27dae750906c0132d426eccd8774b0d08f843dec
Stephan Bösebeck <sb@caluga.de>
2022-10-20 22:03:30 +0200

    back to hjkl for moving?

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -16,110 +16,7 @@ if not status_ok then
 	--   visual_block_mode = "x",
 	--   term_mode = "t",
 	--   command_mode = "c",
-
-	-- Normal --
-	-- Better window navigation
-	keymap("n", "<leader>wj", "<C-w>h", opts)
-	keymap("n", "<leader>wk", "<C-w>j", opts)
-	keymap("n", "<leader>wi", "<C-w>k", opts)
-	keymap("n", "<leader>wl", "<C-w>l", opts)
-	keymap("n", "<leader>j", "<C-w>h", opts)
-	keymap("n", "<leader>k", "<C-w>j", opts)
-	keymap("n", "<leader>i", "<C-w>k", opts)
-	keymap("n", "<leader>l", "<C-w>l", opts)
-
-	-- Resize with arrows
-	keymap("n", "<M-Up>", ":resize -2<CR>", opts)
-	keymap("n", "<M-Down>", ":resize +2<CR>", opts)
-	keymap("n", "<M-Left>", ":vertical resize -2<CR>", opts)
-	keymap("n", "<M-Right>", ":vertical resize +2<CR>", opts)
-
-	-- Navigate buffers
-	keymap("n", "<S-l>", ":bnext<CR>", opts)
-	keymap("n", "<S-k>", ":bprevious<CR>", opts)
-	keymap("n", "<S-c>", ":close<CR>", opts)
-
-	-- Clear highlights
-	keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
-
-	-- Close buffers
-	keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
-
-	-- Better paste
-	keymap("v", "p", '"_dP', opts)
-
-	-- Insert --
-	-- Press jk fast to enter
-	--keymap("i", "jk", "<ESC>", opts)
-
-	-- Visual --
-	-- Stay in indent mode
-	keymap("v", "<", "<gv", opts)
-	keymap("v", ">", ">gv", opts)
-
-	-- Plugins --
-
-	-- NvimTree
-	keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
-
-	-- Telescope
-	keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-	keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
-	keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
-	keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-
-	-- Git
-	keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
-
-	-- Comment
-	-- keymap("n", "<leader>gcc", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
-	-- keymap("x", "<leader>gcc", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>')
-
-	-- DAP
-	keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
-	keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
-	keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
-	keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", opts)
-	keymap("n", "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", opts)
-	keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
-	keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
-	keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
-	keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
-
-	-- -- lsp
-	keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	keymap("n", "gI", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-	keymap("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
-	keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-	keymap("n", "<leader>li", "<cmd>LspInfo<cr>", opts)
-	-- keymap("n", "<leader>lI", "<cmd>LspInstallInfo<cr>", opts)
-	keymap("n", "<leader>la", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-	keymap("n", "<leader>lj", "<cmd>lua vim.diagnostic.goto_next({buffer=0})<cr>", opts)
-	keymap("n", "<leader>lk", "<cmd>lua vim.diagnostic.goto_prev({buffer=0})<cr>", opts)
-	keymap("n", "<leader>lr", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-	keymap("n", "<leader>ls", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-	keymap("n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-
-	-- Toggleterm
-	keymap("n", "<F12>", "<cmd>ToggleTerm direction=float<cr>", opts)
-	keymap("n", "<F10>", "<cmd>ToggleTerm direction=horizontal<cr>", opts)
-
-	-- spectre
-	keymap("n", "<leader>S", "<cmd>lua require('spectre').open()<CR>", opts)
-	keymap("n", "<leader>sw", "<cmd>lua require('spectre').open_visual({select_word=true})<CR>", opts)
-	keymap("n", "<leader>s", "<esc>:lua require('spectre').open_visual()<CR>", opts)
-	keymap("n", "<leader>sp", "viw:lua require('spectre').open_file_search()<cr>", opts)
-
-	-- Toggleterm
-	keymap("n", "<F12>", "<cmd>ToggleTerm direction=float<cr>", opts)
-	keymap("n", "<F10>", "<cmd>ToggleTerm direction=horizontal<cr>", opts)
-	-- debugging
-	keymap("n", "<F8>", "<cmd>lua require('dap').step_over()<CR>", opts)
-	keymap("n", "<F9>", "<cmd>lua require('dap').continue()<CR>", opts)
-	keymap("n", "<F7>", "<cmd>lua require('dap').step_into()<CR>", opts)
+	print("Cannot set keys without which-key")
 else
 	-- print("Configuring which-key")
 	-- local opts = {
@@ -174,7 +71,7 @@ else
 			q = { "<cmd>lua vim.diagnostic.setloclist()<CR>", "LocList" },
 			d = { "<cmd>lua vim.diagnostic.open_float()<cr>", "diagnostic" },
 		},
-	}, { mode = "n", prefix = "" })
+	}, { mode = "n", prefix = "<leader>" })
 
 	wk.register({
 		t = {
@@ -219,11 +116,16 @@ else
 	}, { mode = "n", prefix = "<leader>" })
 
 	-- Buffers
+	-- wk.register({
+	-- 	L = { ":bnext<CR>", "next buffer" },
+	-- 	K = { ":bprevious<CR>", "previous buffer" },
+	-- 	C = { ":close<CR>", "close buffer" },
+	-- 	Q = { "<cmd>Bdelete!<CR>", "Delete Buffer" },
+	-- }, { mode = "n", prefix = "" })
+
 	wk.register({
+		H = { ":bprevious<CR>", "previous buffer" },
 		L = { ":bnext<CR>", "next buffer" },
-		K = { ":bprevious<CR>", "previous buffer" },
-		C = { ":close<CR>", "close buffer" },
-		Q = { "<cmd>Bdelete!<CR>", "Delete Buffer" },
 	}, { mode = "n", prefix = "" })
 
 	-- NvimTree
@@ -234,20 +136,19 @@ else
 	-- Git
 	-- keymap("n", "<leader>gg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", opts)
 
-
 	keymap("v", "<S-TAB>", "<gv", opts)
 	keymap("v", "<TAB>", ">gv", opts)
 	keymap("n", "<TAB>", ">>", opts)
 	keymap("n", "<S-TAB>", "<<", opts)
 
-  wk.register({
-    w={
-      j={":wincmd h<cr>","Window to the left"},
-      k={":wincmd j<cr>","Window to the right"},
-      i={":wincmd k<cr>","Window down"},
-      l={":wincmd l<cr>","Window up"},
-    }
-  },{mode="n",prefix="<leader>"})
+	wk.register({
+		w = {
+			h = { ":wincmd h<cr>", "Window to the left" },
+			j = { ":wincmd j<cr>", "Window to the right" },
+			k = { ":wincmd k<cr>", "Window down" },
+			l = { ":wincmd l<cr>", "Window up" },
+		},
+	}, { mode = "n", prefix = "<leader>" })
 	-- keymap("n", "<C-j>", "<cmd>:wincmd h<cr>", opts)
 	-- keymap("n", "<C-k>", "<cmd>:wincmd j<cr>", opts)
 	-- keymap("n", "<C-i>", "<cmd>:wincmd k<CR>", opts)
@@ -263,15 +164,13 @@ else
 	keymap("n", "<M-Left>", ":vertical resize -2<CR>", opts)
 	keymap("n", "<M-Right>", ":vertical resize +2<CR>", opts)
 
-
-  -- hop
-  -- keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", opts)
-  -- keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", opts)
-  -- keymap('n', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })<cr>", opts)
-  -- keymap('n', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })<cr>", opts)
-  keymap('n','f','<cmd>:HopChar1<cr>',opts)
-  keymap('n','t','<cmd>:HopChar2<cr>',opts)
-  keymap('n', '<leader>F', "<cmd>:HopWord<cr>", opts)
-  keymap('n', '<leader>T', "<cmd>:HopLine<cr>", opts)
-
+	-- hop
+	-- keymap('n', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = true })<cr>", opts)
+	-- keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", opts)
+	-- keymap('n', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false, hint_offset = -1 })<cr>", opts)
+	-- keymap('n', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 })<cr>", opts)
+	keymap("n", "f", "<cmd>:HopChar1<cr>", opts)
+	keymap("n", "t", "<cmd>:HopChar2<cr>", opts)
+	keymap("n", "<leader>F", "<cmd>:HopWord<cr>", opts)
+	keymap("n", "<leader>T", "<cmd>:HopLine<cr>", opts)
 end
