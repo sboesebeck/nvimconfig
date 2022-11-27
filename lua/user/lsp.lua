cda5184fb008bdbe1a8500bde3d4906f5bf53c8d
Stephan Bösebeck <stephan@victorias-mbp.home.local>
2022-11-27 19:13:09 +0100

    adding mason auto config

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -35,6 +35,30 @@ require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { "sumneko_lua", "rust_analyzer", "jdtls" },
 })
+require("mason-lspconfig").setup_handlers({
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name) -- default handler (optional)
+		require("lspconfig")[server_name].setup({})
+	end,
+	-- Next, you can provide a dedicated handler for specific servers.
+	-- For example, a handler override for the `rust_analyzer`:
+	["rust_analyzer"] = function()
+		require("rust-tools").setup({})
+	end,
+	["sumneko_lua"] = function()
+		lspconfig.sumneko_lua.setup({
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
+					},
+				},
+			},
+		})
+	end,
+})
 
 -- python
 -- lspconfig.pyright.setup{}
