e807b83f97b0901b7230da70064152341b9a4f17
Stephan Bösebeck <sb@caluga.de>
2022-12-05 08:56:50 +0100

    some settings

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -36,6 +36,7 @@ vim.opt.wrap = false                            -- display lines as one long lin
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "MesloLGS NF:h17"               -- the font used in graphical neovim applications
+--vim.opt.guicursor = ""
 vim.opt.fillchars.eob=" "
 vim.opt.shortmess:append "c"
 vim.opt.whichwrap:append("<,>,[,],h,l")
